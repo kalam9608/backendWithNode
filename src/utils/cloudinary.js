3340f223e2f407ae20566a0d0c5a4978f3dd18e9
mdkalam <kalam.desuntech@gmail.com>
2024-05-15 00:14:27 +0530

    file upload setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadFileCloudinary=async (filepath)=>{
+    try {
+        if(!filepath) return true;
+
+        //uplaod file to cloudinary 
+
+       const response=await cloudinary.uploader.upload(filepath,{resource_type:"auto"})
+
+        console.log("file uplaod in successfully")
+        
+        return response
+    } catch (error) {
+        fs.unlinkSync(filepath) // removed the loacal save temprory filed uplaod file got failed
+    }
+}
+
+export {cloudinary}
+
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
